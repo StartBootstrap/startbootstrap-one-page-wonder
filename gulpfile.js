d5d856e4691239d06fe970ac38738ee1105e381c
David Miller <hello@davidmiller.io>
2017-12-14 16:04:09 -0500

    bootstrap 4 design update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var header = require('gulp-header');
+var rename = require("gulp-rename");
 var browserSync = require('browser-sync').create();
 var pkg = require('./package.json');
 
+// Set the banner content
+var banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  ''
+].join('');
+
 // Copy vendor files from /node_modules into /vendor
-// NOTE: requires `npm install` before running!
 gulp.task('copy', function() {
   gulp.src([
       'node_modules/bootstrap/dist/**/*',
@@ -17,21 +29,50 @@ gulp.task('copy', function() {
     .pipe(gulp.dest('vendor/jquery'))
 })
 
+// Compile SCSS
+gulp.task('css:compile', function() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('./css'))
+});
+
+// Minify CSS
+gulp.task('css:minify', ['css:compile'], function() {
+  return gulp.src([
+      './css/*.css',
+      '!./css/*.min.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+// CSS
+gulp.task('css', ['css:compile', 'css:minify']);
+
 // Default task
-gulp.task('default', ['copy']);
+gulp.task('default', ['css', 'copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: ''
-    },
-  })
-})
+      baseDir: "./"
+    }
+  });
+});
 
-// Dev task with browserSync
-gulp.task('dev', ['browserSync'], function() {
-  // Reloads the browser whenever HTML or CSS files change
-  gulp.watch('css/*.css', browserSync.reload);
+// Watch task
+gulp.task('dev', ['css', 'browserSync'], function() {
+  gulp.watch('./scss/*.scss', ['css']);
+  // Reloads the browser whenever HTML files change
   gulp.watch('*.html', browserSync.reload);
 });
