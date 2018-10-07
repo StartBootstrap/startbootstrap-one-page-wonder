712c77ed29a91df6e55303feaf518402600ab202
flamebeard <ethan@hursty.co.nz>
2018-10-08 12:29:00 +1300

    changed from class to className

--- src/components/layout/_Footer.jsx
+++ src/components/layout/_Footer.jsx
@@ -4,9 +4,9 @@ export default class _Footer extends React.Component {
     render() {
         return (
             <div className="Footer">
-                <footer class="py-5 bg-black">
-                    <div class="container">
-                        <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2018   </p>
+                <footer className="py-5 bg-black">
+                    <div className="container">
+                        <p className="m-0 text-center text-white small">Copyright &copy; Your Website 2018   </p>
                     </div>
                 </footer>
             </div>
