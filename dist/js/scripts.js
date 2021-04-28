46cd9bb94f3704c6f1fb5a421c23dc491e1bd0a2
David Miller <hello@davidmiller.io>
2021-04-28 15:11:20 -0400

    Bootstrap 4.6.0 and dev environment update

---
+++ dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - One Page Wonder v5.1.0 (https://startbootstrap.com/theme/one-page-wonder)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
